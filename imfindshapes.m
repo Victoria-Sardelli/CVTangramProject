7090e93c9285a790605cb760335490628441216e
roxymeskell <roxy.meskell@gmail.com>
2017-11-29 14:27:16 -0500

    Update imfindshapes to get hough shape transform based on just edges

--- imfindshapes.m
+++ imfindshapes.m
@@ -17,38 +17,80 @@ flip = parsedInputs.Flip;
 shapeTP = parsedInputs.ShapeTemplate;
 edgeThresh = parsedInputs.EdgeThreshold;
 
-    %% Convert image to grayscale
-    I = rgb2gray(I);
+    %% Rotate shape template 180 degrees round origin
+    %shapeM = rotz(180) * shapeTP;
+    shapeM = -1 * shapeTP;
+    
+    %% Get max width of shape for padding and angles of edges...?
+    % acosd
+    pointCt = size(shapeM,2);
+    maxLineL = 0;
+    lineAngles = zeros(1, factorial(pointCt)/(2 * factorial(pointCt-2)));
+    i = 1;
+    for a = [1:pointCt-1]
+        for b = [a+1:pointCt]
+            lineL = sqrt(sum((shapeM(:,a) - shapeM(:,b)).^2));
+            lineA = acosd(abs(shapeM(2,a) - shapeM(2,b))/lineL);
+            if maxLineL < lineL
+                maxLineL = lineL;
+            end
+            lineAngles(i) = lineA;
+            i = i + 1;
+        end
+    end
+    imPadding = ceil(maxLineL * max(sizeRange));
+    lineAngles = unique(lineAngles);
+    
 
-    %% Get image edges
-    Iedges = edge(I,'Canny');
+    %% Convert image to grayscale, if needed, and get image size
+    if (size(I,3) > 1)
+        I = rgb2gray(I);
+    end
+    [Iheight, Iwidth] = size(I);
+
+    %% Get image edges, and pad with zeros
+    Iedges = edge(I,'sobel');
     Iedges = im2double(Iedges);
+    Iedges = imdilate(Iedges, ones(3)); % Dilate to thicken edges
+    %Iedges = imrotate(Iedges,180);
+    IedgesPadded = padarray(Iedges, [imPadding imPadding], 0, 'post');
     
     %% Get size and rotation ranges
     if (length(sizeRange) > 1)
-        sizeRange = minSize:sizeInc:max(sizeRange);
+        sizeRange = min(sizeRange):sizeInc:max(sizeRange);
     end
-    if (length(rotRange) > 1)
+    if (length(rotRange) == 2)
         rotRange = min(rotRange):rotInc:max(rotRange);
     end
     
-    %% Get shape template size
-    shapeTPSize = max(sum(shapeTP));
-    
     %% Create voting space and vote
-    houghtrans = zeros(size(Iedges,1), size(Iedges,2),...
-        length(sizeRange), length(rotRange));
+    %houghtrans = zeros(size(Iedges,1), size(Iedges,2),...
+    %    length(sizeRange), length(rotRange));
+    houghtrans = zeros(Iheight, Iwidth,...
+        length(rotRange), length(sizeRange));
     for si = [1:length(sizeRange)]
         s = sizeRange(si);
-        resizedShape = imresize(shapeTP, s/shapeTPSize);
         for ri = [1:length(rotRange)]
             r = rotRange(ri);
-            rotShape = imrotate(resizedShape, r);
-            shapeEdges = im2double(edge(rotShape, 'Canny'));
-            votingSpace = imfilter(Iedges, shapeEdges);
-            houghtrans(:, :, si, ri) = votingSpace;
+            tShapeM = s * rotz(r) * shapeTP;
+            tShapeM(3,:) = []; % Get rid of z-coordinates
+            for p = tShapeM % Loop over points
+                IedgesShift = circshift(IedgesPadded, round(p));
+                % There is probably a more accurate option than rounding p
+                IedgesShift = IedgesShift(1:Iheight, 1:Iwidth);
+                %f = figure, subplot(2,1,1), imshow(Iedges),
+                %subplot(2,1,2), imshow(IedgesShift);
+                %close(f);
+                houghtrans(:, :, ri, si) = houghtrans(:, :, ri, si) + IedgesShift;
+            end
+            % smooth voting space
+            %h = fspecial('gaussian',floor(s / 2),2);
+            %houghtrans(:, :, ri, si) = imfilter(houghtrans(:, :, ri, si), h);
         end
     end
+    
+    % NEED TO FACTOR IN FILP AND GRADIENTS
+    % imgradientxy(I, 'sobel');
 
 end
 
@@ -71,7 +113,9 @@ if (isempty(parser))
     %parser.addParamValue('ObjectPolarity','bright');
     parser.addParamValue('RotationRange',[0 90],@checkRotRange);
     parser.addParamValue('Flip',0,@checkFlip);
-    parser.addParamValue('ShapeTemplate',[1 1; 1 1]); % @checkShapeTemplate
+    %parser.addParamValue('ShapeTemplate',[1 1; 1 1]); % @checkShapeTemplate
+    parser.addParamValue('ShapeTemplate',[-0.5 -0.5 0.5 0.5;...
+        -0.5 0.5 0.5 -0.5; 0 0 0 0]); % @checkShapeTemplate
     parser.addParamValue('SizeIncrement',1);
     parser.addParamValue('RotationIncrement',1);
     parser.addParamValue('EdgeThreshold',[]);
@@ -89,6 +133,7 @@ parsedInputs = parser.Results;
 
 validateSizeRange(); % If Smin and Smax are the same, setS = Smin
 validateRotRange(); % If Rmin and Rmax are the same then set R = Rmin.
+validateShapeTP();
     
     function tf = checkImage(A)
         allowedImageTypes = {'uint8', 'uint16', 'double', 'logical', 'single', 'int16'};
@@ -127,17 +172,19 @@ validateRotRange(); % If Rmin and Rmax are the same then set R = Rmin.
     function tf = checkRotRange(rotRange)
         if (isscalar(rotRange))
             validateattributes(rotRange,{'numeric'},{'nonnan', ...
-                'nonsparse','nonempty','positive','finite','vector'});
+                'nonsparse','nonempty','finite','vector'});
         else
             validateattributes(rotRange,{'numeric'},{'integer','nonnan', ...
-                'nonsparse','nonempty','positive','finite','vector'});
+                'nonsparse','nonempty','finite','vector'});
         end
-        if (length(rotRange) > 2)
-            error(message('tangram:imfindshapes:unrecognizedSizeRange'));
-        elseif (length(rotRange) == 2)
-            if (rotRange(1) > rotRange(2))
+        %if (length(rotRange) > 2)
+        %    error(message('tangram:imfindshapes:unrecognizedSizeRange'));
+        %else
+        if max(rotRange) > 359
                 error(message('images:imfindshapes:invalidSizeRange'));
-            elseif rotRange(2) > 359
+        end
+        if (length(rotRange) == 2)
+            if (rotRange(1) > rotRange(2))
                 error(message('images:imfindshapes:invalidSizeRange'));
             end
         end
@@ -146,8 +193,7 @@ validateRotRange(); % If Rmin and Rmax are the same then set R = Rmin.
     end
 
     function tf = checkFlip(flip)
-        validateattributes(flip,{'logical'},{'nonnan', ...
-            'nonsparse','nonempty','positive','finite','vector'},mfilename,'RADIUS_RANGE',2);
+        validateattributes(flip,{'logical'});
         tf = true;
     end
 
@@ -164,6 +210,13 @@ validateRotRange(); % If Rmin and Rmax are the same then set R = Rmin.
             if (parsedInputs.RotationRange(1) == parsedInputs.RotationRange(2))
                 parsedInputs.RotationRange = parsedInputs.RotationRange(1);
             end
+        end
+    end
+
+    function validateShapeTP
+        if (size(parsedInputs.RotationRange,1) == 2)
+            parsedInputs.RotationRange(3,:) = zeros(1,...
+                size(parsedInputs.RotationRange,2));
         end
     end
         
